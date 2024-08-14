a55be646052be4cbced4ecfa5805cc7d3ccc3cb4
Sam Li <slipoe@outlook.com>
2024-08-14 09:59:48 -0700

    Add sprockets

---
+++ PerfectWebApp.Core/Entities/Sprocket.cs
@@ -0,0 +1,9 @@
+﻿namespace PerfectWebApp.Core.Entities
+{
+    public class Sprocket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
--- PerfectWebApp.Web/Controllers/ProductsController.cs
+++ PerfectWebApp.Web/Controllers/ProductsController.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PerfectWebApp.Core.Entities;
 using PerfectWebApp.Core.Interfaces;
+using PerfectWebApp.Data.Repositories;
 
 namespace PerfectWebApp.Web.Controllers
 {
     public class ProductsController : Controller
     {
+        private const string SprocketConnectionString = "Server=...";
         private readonly ILogger<ProductsController> _logger;
         private readonly IWidgetRepository _widgetRepository;
+        private readonly SprocketRepository _sprocketRepository;
 
         public ProductsController(ILogger<ProductsController> logger, IWidgetRepository widgetRepository)
         {
             _logger = logger;
             _widgetRepository = widgetRepository;
+            _sprocketRepository = new SprocketRepository(SprocketConnectionString);
         }
 
         public IActionResult Index()
@@ -23,6 +28,30 @@ namespace PerfectWebApp.Web.Controllers
         {
             var widgets = _widgetRepository.GetWidgets();
             return View(widgets);
+        }
+
+        public async Task<IActionResult> Sprockets()
+        {
+            var sprockets = await _sprocketRepository.GetSprocketsAsync();
+            Process(sprockets.ToList());
+            return View(sprockets);
+        }
+
+        private async void Process(List<Sprocket> sprockets)
+        {
+            foreach (var s in sprockets)
+            {
+                if (s.Description.Contains("flange"))
+                {
+                    // process flange
+                    await Task.Delay(100);
+                }
+                else
+                {
+                    // process sprocket
+                    await Task.Delay(500);
+                }
+            }
         }
     }
 }
